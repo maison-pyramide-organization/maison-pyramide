0634db76c62e3225d0a21d69e3488ea66bb354d6
maryamfarrag0 <maryamfarrag0@gmail.com>
2022-03-17 16:10:51 +0200

    iccooonopdsakcds

--- pages/_app.js
+++ pages/_app.js
@@ -62,7 +62,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.svg" />
       <link rel="manifest" href="/manifest.json" />
       {/* <link rel="apple-touch-icon" href="/imgs/icon.svg" /> */}
     </Head>
