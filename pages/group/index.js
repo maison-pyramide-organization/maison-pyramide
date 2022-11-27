fafb73a990d5a0b159de6a3f2fc34c6d25c985df
Mahmoud Mohamed <mahmoudmo.mustafa@gmail.com>
2022-11-27 13:38:01 +0200

    fix group

--- pages/group/index.js
+++ pages/group/index.js
@@ -200,46 +200,13 @@ export default function Group() {
           </Row>
           </Container>
 
-          <Container>
-          <h2 className="text-center">Maison Pyramide Group</h2>
-          <Row className={groupStyles.maisonGroup}>
-            <Col md={4}>
-            <div className={groupStyles.card}>
-            <h3>Maison Pyramide</h3>
-                <span>Maison Pyramide is the strategic branch of the company supporting the pace of innovation and growth of young and established entities looking to expand and grow.</span>
-                </div>
-            </Col>
-            <Col md={4}>
-            <div className={groupStyles.card}>
-            <h3><a href="https://eshowroom.maisonpyramide.com/" target={"_blank"} rel="noreferrer">The Showroom</a></h3>
-                <span>The Showroom is Maison Pyramide Group’s wholesale and retail strategy, sales, and distribution platform. We support multinational brands launching new retail opportunities or expanding their distribution.</span>
-                <p className="pt-3">
-                <a href="mailto:showroom@maisonpyramide.com"
-                    target="_blank" rel="noreferrer" className={groupStyles.mail}>
-                      showroom@maisonpyramide.com</a>
-            </p>
-                </div>
-            </Col>
-            <Col md={4}>
-            <div className={groupStyles.card}>
-            <h3><a href="https://www.instagram.com/egoandeast/" target={"_blank"} rel="noreferrer">Ego & East</a></h3>
-            <span>A talent management agency representing celebrities, social media influencers, actors, and musicians. Ego & East partners global brands with the right digital talents.</span>
-            <p className="pt-3">
-                <a href="mailto:hello@egoandeast.co"
-                    target="_blank" rel="noreferrer" className={groupStyles.mail}>
-                      hello@egoandeast.co</a>
-            </p>
-                </div>
-            </Col>
-          </Row>
-          </Container>
           </Parallax>
           {/* </ParallaxProvider> */}
 
           <Container fluid>
           {/* <ParallaxProvider> */}
           {/* <ParallaxCache/> */}
-          <Parallax translateY={[isMobile?10:30, isMobile?-10:-30,'easeOutQuint']}>
+          <Parallax translateY={[isMobile?10:20, isMobile?-10:-10,'easeOutQuint']}>
             <ParallaxCache/>
             <div className={groupStyles.office}>
               <Row>
@@ -283,7 +250,7 @@ export default function Group() {
 
           {/* <ParallaxProvider>
           <ParallaxCache/> */}
-          <Parallax translateY={[isMobile?-1:10, isMobile?-7:-30]}>
+          <Parallax translateY={[isMobile?-1:10, isMobile?-7:-20]}>
           <ParallaxCache/>
             <div className={groupStyles.leader}>
               <motion.h2 className="text-center pb-3"
@@ -407,7 +374,7 @@ export default function Group() {
 
           <ParallaxProvider>
           <ParallaxCache/>
-          <Parallax translateY={[isMobile?-11:10, isMobile?-15:-30]}>
+          <Parallax translateY={[isMobile?-20:10, isMobile?-15:-30]}>
             {data?.slider && 
             (
               <GroupSlider cultureText={data?.culture_text} valuesText={data?.values_text} images={data?.slider}/>
