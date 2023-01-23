bed6b8c4b10aee1a46893dbbcd6c40d656141657
m0bad <mobad.cai@gmail.com>
2023-01-23 11:13:26 +0200

    changed homepage video.

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ function Home() {
 
       <video autoPlay playsInline muted loop id="myVideo" style={{width:'100%' , height:'100vh',objectFit:'cover'}}>
         {/* <source src={isMobile?"/imgs/videobgmobile.mp4":"/imgs/videobg.mp4"} /> */}
-        <source src={isMobile?"https://gazef.s3.us-west-2.amazonaws.com/maison/videobgmobile.mp4":"/imgs/videobg.mp4"} />
+          <source src={isMobile ? "https://res.cloudinary.com/dhqpdvngy/video/upload/q_auto,f_auto/c_scale/website_vertical_zothcm.mp4": "https://res.cloudinary.com/dhqpdvngy/video/upload/q_auto,f_auto/c_scale/website_video_2023_1_t0lyme.mp4"} />
       </video>
       )}
       {/* {!isMobile && (
